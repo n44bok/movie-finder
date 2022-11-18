f9a1da07f1874d7eb6a8ca532503bc491be1669f
n44bok <vamromanov@gmail.com>
2022-11-18 12:21:27 +0100

    end

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,75 +1,73 @@
 // import { Routes, Route } from 'react-router-dom';
 
 import React, { Component } from "react";
-// import Nav from '../Navigation/Nav';
-// import SearchArea from "components/SearchArea/SearchArea";
-// import MovieList from "components/MovieList/MovieList";
-// import Pagination from "components/Pagination/Pagination";
-// import MovieInfo from "components/MovieInfo/MovieInfo";
+import Nav from '../Navigation/Nav';
+import SearchArea from "components/SearchArea/SearchArea";
+import MovieList from "components/MovieList/MovieList";
+import Pagination from "components/Pagination/Pagination";
+import MovieInfo from "components/MovieInfo/MovieInfo";
 
 class App extends Component {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     movies: [],
-  //     searchTerm: '',
-  //     totalResults: 0,
-  //     currentPage: 1,
-  //     currentMovie: null
-  //   }
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      searchTerm: '',
+      totalResults: 0,
+      currentPage: 1,
+      currentMovie: null
+    }
     
-  //   this.api_key = '21fbb343042b43d66a461193a2d3041e';
-  // }
+    this.api_key = '21fbb343042b43d66a461193a2d3041e';
+  }
 
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-  //   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${this.state.searchTerm}`)
-  //     .then(data => data.json())
-  //     .then(data => {
-  //       console.log(data);
-  //       this.setState({ movies: [...data.results], totalResults: data.total_results})
-  //     })
-  // }
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${this.state.searchTerm}`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ movies: [...data.results], totalResults: data.total_results})
+      })
+  }
 
 
-  // handleChange = (e) => {
-  //   this.setState({searchTerm: e.target.value})
-  // }
+  handleChange = (e) => {
+    this.setState({searchTerm: e.target.value})
+  }
 
-  // nextPage = (pageNumber) => {
-  //   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${this.state.searchTerm}&page=${pageNumber}`)
-  //     .then(data => data.json())
-  //     .then(data => {
-  //       console.log(data);
-  //       this.setState({ movies: [...data.results], currentPage: pageNumber})
-  //     })
-  // }
+  nextPage = (pageNumber) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${this.state.searchTerm}&page=${pageNumber}`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ movies: [...data.results], currentPage: pageNumber})
+      })
+  }
 
-  // viewMovieInfo = (id) => {
-  //   const filteredMovie = this.state.movies.filter(movie => movie.id == id)
+  viewMovieInfo = (id) => {
+    const filteredMovie = this.state.movies.filter(movie => movie.id == id)
 
-  //   const newCurrentMovie = filteredMovie.length > 0 ? filteredMovie[0] : null
+    const newCurrentMovie = filteredMovie.length > 0 ? filteredMovie[0] : null
 
-  //   this.setState({currentMovie: newCurrentMovie})
+    this.setState({currentMovie: newCurrentMovie})
 
-  // }
+  }
 
-  // closeMovieInfo = () => {
-  //   this.setState({currentMovie: null})
-  // }
+  closeMovieInfo = () => {
+    this.setState({currentMovie: null})
+  }
 
 
   render() {
-    // const numberPages = Math.floor(this.state.totalResults / 20);
+    const numberPages = Math.floor(this.state.totalResults / 20);
     return (
-      <div className="App"><p>Privet</p>
-        <p>Poka</p>
-        <p>Hui</p>
-        <p>Pizda</p>
-        {/* <Nav />
+      <div className="App">
+        
+        <Nav />
         {this.state.currentMovie == null ? <div><SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange} /><MovieList viewMovieInfo={this.viewMovieInfo} movies={this.state.movies} /></div> : <MovieInfo currentMovie={this.state.currentMovie} closeMovieInfo={this.closeMovieInfo} />}
-        { this.state.totalResults > 20 && this.state.currentMovie == null ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage}/>  : '' } */}
+        { this.state.totalResults > 20 && this.state.currentMovie == null ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage}/>  : '' }
       </div>
     );
   }
